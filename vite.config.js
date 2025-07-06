c304c4c64244dfc3502df23961fbc3ee5b2ef241
SutharHarsh <sutharharshp01@gmail.com>
2025-07-06 11:26:56 +0530

    Change vite configuration to the old configuration

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://uke-strapi-backend.onrender.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
 })
